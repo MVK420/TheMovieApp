188288b50c02a255a2d52b22ef751c0ee559605e
MozesVidami <mozeskec@gmail.com>
2021-01-17 22:31:56 +0200

    Initial Commit

---
+++ TheMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieApp
+//
+//  Created by Mozes Vidami on 1/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
