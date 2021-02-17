ce3f4d0fa2e52da6e9087e94a6815448f0764f38
MozesVidami <mozeskec@gmail.com>
2021-02-17 15:44:13 +0200

    Removed genrestack view code

--- TheMovieApp/ViewControllers/DetailsViewController.swift
+++ TheMovieApp/ViewControllers/DetailsViewController.swift
@@ -7,18 +7,8 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.genres.count
-    }
+class DetailsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,  UICollectionViewDelegateFlowLayout {
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "genreCell", for: indexPath) as? GenreCell else { return UICollectionViewCell() }
-        cell.setupLabel(viewModel.genres[indexPath.row])
-        return cell
-    }
-    
-    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var overviewLabel: UILabel!
@@ -26,9 +16,7 @@ class DetailsViewController: UIViewController, UICollectionViewDelegate, UIColle
     @IBOutlet weak var popularityLabel: UILabel!
     @IBOutlet weak var languageLabel: UILabel!
     @IBOutlet weak var genreCollectionView: UICollectionView!
-    
-    
-    var viewModel: DetailViewModel!
+    private var viewModel: DetailViewModel!
     var model:Movie! {
         didSet {
             viewModel = DetailViewModel(with: model)
@@ -48,10 +36,6 @@ class DetailsViewController: UIViewController, UICollectionViewDelegate, UIColle
         statusLabel.text = viewModel.status
         languageLabel.text = viewModel.language
         popularityLabel.text = viewModel.popularity
-//        for genre in viewModel.genres {
-//            let label = createGenreLabel(genre)
-//            genreStackView.addArrangedSubview(label)
-//        }
         if viewModel.imgURL.absoluteString == Strings.noImageError {
             posterImageView.backgroundColor = .red
         } else {
@@ -59,17 +43,13 @@ class DetailsViewController: UIViewController, UICollectionViewDelegate, UIColle
         }
     }
     
-    private func createGenreLabel(_ text: String) -> UILabel {
-        let label = UILabel()
-        label.text = text
-        label.frame.size.width = 67
-        label.font = UIFont.boldSystemFont(ofSize: 12)
-        label.textColor = .white
-        label.textAlignment = .center
-        label.backgroundColor = UIColor(red: 1.0, green: 0, blue: 0, alpha: 1.0)
-        label.layer.cornerRadius = 5
-        label.layer.masksToBounds = true
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.genres.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "genreCell", for: indexPath) as? GenreCell else { return UICollectionViewCell() }
+        cell.setupLabel(viewModel.genres[indexPath.row])
+        return cell
     }
 }
